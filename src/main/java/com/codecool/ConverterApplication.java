2ef6b2cac4758390551481388f59bc9c8de92b99
Maciej Nowak <mcnowak@wp.pl>
2017-12-28 22:23:01 +0100

    Handle no csv file given situation in main method of ConverterApplication

--- src/main/java/com/codecool/ConverterApplication.java
+++ src/main/java/com/codecool/ConverterApplication.java
@@ -1,8 +1,14 @@
 package com.codecool;
 
 public class ConverterApplication {
-    
+
     public static void main(String[] args) {
 
+        if (args.length == 0) {
+            System.out.println("No input file defined");
+
+        } else {
+            
+        }
     }
 }
