af3b8662cd9b55cabc6568d51702950da6dddfd1
Maciej Nowak <mcnowak@wp.pl>
2017-12-29 23:15:45 +0100

    Handle output formats and filepath given from command line in ConverterApplication main method

--- src/main/java/com/codecool/ConverterApplication.java
+++ src/main/java/com/codecool/ConverterApplication.java
@@ -1,14 +1,24 @@
 package com.codecool;
 
+import com.codecool.enums.OutputFormats;
+
+import java.io.File;
+
 public class ConverterApplication {
 
     public static void main(String[] args) {
+        Integer argumentsCount = args.length;
 
-        if (args.length == 0) {
+        if (argumentsCount == 0) {
             System.out.println("No input file defined");
 
+        } else if (argumentsCount == 1 && new File(args[0]).isFile()){
+            System.out.println("File has been found!");
+
+        } else if (argumentsCount > 1 && new File(args[1]).isFile() && OutputFormats.contains(args[0])) {
+            System.out.println("Proper format and file have been found!");
         } else {
-            
+            System.out.println();
         }
     }
 }
