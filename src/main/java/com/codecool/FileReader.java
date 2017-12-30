aeba904bc88d07e17b3545f695dd6760e25c1773
Maciej Nowak <mcnowak@wp.pl>
2017-12-30 23:15:31 +0100

    Add read method in FileReader class

--- src/main/java/com/codecool/FileReader.java
+++ src/main/java/com/codecool/FileReader.java
@@ -1,4 +1,14 @@
 package com.codecool;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileReader {
+
+    public List<String> read(File file) {
+        List<String> fileContent = new ArrayList<>();
+        System.out.println("File has been read");
+        return fileContent;
+    }
 }
