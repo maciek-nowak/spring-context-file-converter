15e9941fbec2774fc24ab0272a3afe287018766d
Maciej Nowak <mcnowak@wp.pl>
2017-12-30 23:36:53 +0100

    Create constructor of  SimpleCsvConverter class. Make FileReader object a field of SimpleCsvConverter

--- src/main/java/com/codecool/SimpleCsvConverter.java
+++ src/main/java/com/codecool/SimpleCsvConverter.java
@@ -4,6 +4,12 @@ import com.codecool.enums.OutputFormats;
 
 public class SimpleCsvConverter {
 
+    private FileReader fileReader;
+
+    public SimpleCsvConverter(FileReader fileReader) {
+        this.fileReader = fileReader;
+    }
+
     public void convert(String file, OutputFormats outputFormat) {
         System.out.println("I convert CSV to output format:" + outputFormat);
     }
