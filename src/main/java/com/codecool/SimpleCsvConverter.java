af6d820dfd060d2dc3176dd9390207f02b211290
Maciej Nowak <mcnowak@wp.pl>
2017-12-30 19:29:08 +0100

    Create  convert method and its overloaded variant in SimpleCsvConverter

--- src/main/java/com/codecool/SimpleCsvConverter.java
+++ src/main/java/com/codecool/SimpleCsvConverter.java
@@ -1,4 +1,15 @@
 package com.codecool;
 
+import com.codecool.enums.OutputFormats;
+
 public class SimpleCsvConverter {
+
+    public void convert(String file, OutputFormats outputFormat) {
+        System.out.println("I convert CSV to output format:" + outputFormat);
+    }
+
+    public void convert(String file) {
+        OutputFormats outputFormat = OutputFormats.TABLE;
+        convert(file, outputFormat);
+    }
 }
