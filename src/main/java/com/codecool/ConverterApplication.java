a028da62db9a5b9fc22b5dd46f10815b71a8a993
Maciej Nowak <mcnowak@wp.pl>
2017-12-30 19:54:49 +0100

    Change ConverterApplication class to run SimpleCsvConverter convert method

--- src/main/java/com/codecool/ConverterApplication.java
+++ src/main/java/com/codecool/ConverterApplication.java
@@ -8,17 +8,21 @@ public class ConverterApplication {
 
     public static void main(String[] args) {
         Integer argumentsCount = args.length;
+        SimpleCsvConverter simpleCsvConverter = new SimpleCsvConverter();
 
         if (argumentsCount == 0) {
             System.out.println("No input file defined");
 
         } else if (argumentsCount == 1 && new File(args[0]).isFile()){
             System.out.println("File has been found!");
+            simpleCsvConverter.convert(args[0]);
 
         } else if (argumentsCount > 1 && new File(args[1]).isFile() && OutputFormats.contains(args[0])) {
             System.out.println("Proper format and file have been found!");
+            simpleCsvConverter.convert(args[1], OutputFormats.getOutputFormat(args[0]));
+
         } else {
-            System.out.println();
+            System.out.println("No proper arguments found! Try again!");
         }
     }
 }
