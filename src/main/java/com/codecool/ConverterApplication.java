358cadafd0afab59c5ae7225bc135411f567f83d
Maciej Nowak <mcnowak@wp.pl>
2018-01-01 22:38:56 +0100

    Add OutputFormatterFactory object as field in SimpleCsvConverter

--- src/main/java/com/codecool/ConverterApplication.java
+++ src/main/java/com/codecool/ConverterApplication.java
@@ -1,6 +1,8 @@
 package com.codecool;
 
 import com.codecool.enums.OutputFormats;
+import com.codecool.view.OutputFormatter;
+import com.codecool.view.OutputFormatterFactory;
 
 import java.io.File;
 
@@ -8,18 +10,20 @@ public class ConverterApplication {
 
     public static void main(String[] args) {
         Integer argumentsCount = args.length;
-        SimpleCsvConverter simpleCsvConverter = new SimpleCsvConverter();
+        FileReader fileReader = new FileReader();
+        OutputFormatterFactory outputFormatterFactory = new OutputFormatterFactory();
+        SimpleCsvConverter simpleCsvConverter = new SimpleCsvConverter(fileReader, outputFormatterFactory);
 
         if (argumentsCount == 0) {
             System.out.println("No input file defined");
 
         } else if (argumentsCount == 1 && new File(args[0]).isFile()){
             System.out.println("File has been found!");
-            simpleCsvConverter.convert(args[0]);
+            simpleCsvConverter.convert(new File(args[0]));
 
         } else if (argumentsCount > 1 && new File(args[1]).isFile() && OutputFormats.contains(args[0])) {
             System.out.println("Proper format and file have been found!");
-            simpleCsvConverter.convert(args[1], OutputFormats.getOutputFormat(args[0]));
+            simpleCsvConverter.convert(new File(args[1]), OutputFormats.getOutputFormat(args[0]));
 
         } else {
             System.out.println("No proper arguments found! Try again!");
--- src/main/java/com/codecool/SimpleCsvConverter.java
+++ src/main/java/com/codecool/SimpleCsvConverter.java
@@ -1,20 +1,29 @@
 package com.codecool;
 
 import com.codecool.enums.OutputFormats;
+import com.codecool.view.OutputFormatter;
+import com.codecool.view.OutputFormatterFactory;
+
+import java.io.File;
+import java.util.List;
 
 public class SimpleCsvConverter {
 
     private FileReader fileReader;
+    private OutputFormatterFactory outputFormatterFactory;
 
-    public SimpleCsvConverter(FileReader fileReader) {
+    public SimpleCsvConverter(FileReader fileReader, OutputFormatterFactory outputFormatterFactory) {
         this.fileReader = fileReader;
+        this.outputFormatterFactory = outputFormatterFactory;
     }
 
-    public void convert(String file, OutputFormats outputFormat) {
+    public void convert(File file, OutputFormats outputFormat) {
         System.out.println("I convert CSV to output format:" + outputFormat);
+        List<String> data = this.fileReader.read(file);
+        OutputFormatter outputFormatter = this.outputFormatterFactory.createByFormat(outputFormat);
     }
 
-    public void convert(String file) {
+    public void convert(File file) {
         OutputFormats outputFormat = OutputFormats.TABLE;
         convert(file, outputFormat);
     }
