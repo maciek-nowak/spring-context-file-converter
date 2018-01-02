446ec3dc2d7ace0ec900a208e58dad19e2044586
Maciej Nowak <mcnowak@wp.pl>
2018-01-02 19:16:59 +0100

    Implement createByFormat method in OutputFormatterFactory class

--- src/main/java/com/codecool/view/OutputFormatterFactory.java
+++ src/main/java/com/codecool/view/OutputFormatterFactory.java
@@ -4,6 +4,19 @@ import com.codecool.enums.OutputFormats;
 
 public class OutputFormatterFactory {
     public OutputFormatter createByFormat(OutputFormats outputFormat){
-        return new JsonOutputFormatter();
+        OutputFormatter outputFormatter = null;
+
+        switch (outputFormat) {
+            case TABLE:
+                outputFormatter = new TableOutputFormatter();
+                break;
+            case XML:
+                outputFormatter = new XmlOutputFormatter();
+                break;
+            case JSON:
+                outputFormatter = new JsonOutputFormatter();
+                break;
+        }
+        return outputFormatter;
     }
 }
