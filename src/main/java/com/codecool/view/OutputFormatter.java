dc830d7e89fc6ea9e9e447d5df9a2345de151d23
Maciej Nowak <mcnowak@wp.pl>
2018-01-03 14:35:15 +0100

    Add default method splitDataRows to OutputFormatter interface

--- src/main/java/com/codecool/view/OutputFormatter.java
+++ src/main/java/com/codecool/view/OutputFormatter.java
@@ -1,8 +1,21 @@
 package com.codecool.view;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface OutputFormatter {
 
     void printToConsole(List<String> data);
+
+    default List<String[]> splitDataRows(List<String> data) {
+
+        List<String[]> splittedRows = new ArrayList<>();
+
+        for (String row : data) {
+            String[] splittedRow = row.split(",");
+            splittedRows.add(splittedRow);
+        }
+
+        return splittedRows;
+    }
 }
