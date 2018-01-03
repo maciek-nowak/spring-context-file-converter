8c0a20ec2e54db00189a65fbe03892896da7dbca
Maciej Nowak <mcnowak@wp.pl>
2018-01-03 14:36:37 +0100

    Implement complet printToConsole method in JsonOutputFormatter class

--- src/main/java/com/codecool/view/JsonOutputFormatter.java
+++ src/main/java/com/codecool/view/JsonOutputFormatter.java
@@ -1,10 +1,43 @@
 package com.codecool.view;
 
 import java.util.List;
+import org.json.JSONObject;
+import org.json.JSONArray;
 
 public class JsonOutputFormatter implements OutputFormatter{
+
     @Override
     public void printToConsole(List<String> data) {
-        System.out.println("Print some Json output");
+        List<String[]> splittedRows = splitDataRows(data);
+        JSONArray jsonArray = buildArray(splittedRows);
+        System.out.println(jsonArray);
+
     }
+
+    private JSONArray buildArray(List<String[]> splittedRows) {
+        JSONArray jsonArray = new JSONArray();
+        String[] columnNames = splittedRows.get(0);
+        splittedRows.remove(0);
+
+        for (String[] row : splittedRows) {
+            JSONObject jsonObject = buildJson(columnNames, row);
+            jsonArray.put(jsonObject);
+        }
+
+        return jsonArray;
+    }
+
+    private JSONObject buildJson(String[] columnNames, String[] row) {
+        JSONObject jsonObject = new JSONObject();
+
+        for (int i = 0; i < columnNames.length; i++) {
+            String key = columnNames[i];
+            String value = row[i];
+            jsonObject.put(key, value);
+        }
+
+        return jsonObject;
+
+    }
+
 }
